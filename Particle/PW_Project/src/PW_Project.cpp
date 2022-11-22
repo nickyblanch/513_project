1b935f53d8a729e98764a5c1459ad02d9d674b2c
Nick Blanchared <nickyblanch@arizona.edu>
2022-11-21 20:16:15 -0700

    firmware working

--- Particle/PW_Project/src/PW_Project.cpp
+++ Particle/PW_Project/src/PW_Project.cpp
@@ -2,8 +2,12 @@
 //       THIS IS A GENERATED FILE - DO NOT EDIT       //
 /******************************************************/
 
-#include "Particle.h"
 #line 1 "c:/Users/nicky/OneDrive/Documents/GitHub/513_project/Particle/PW_Project/src/PW_Project.ino"
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
 // ECE 513
 // Author: Nicolas Blanchard
 
@@ -14,9 +18,9 @@
 #include <Wire.h>
 
 // Definitions
+#line 16 "c:/Users/nicky/OneDrive/Documents/GitHub/513_project/Particle/PW_Project/src/PW_Project.ino"
 void setup();
 void loop();
-#line 11 "c:/Users/nicky/OneDrive/Documents/GitHub/513_project/Particle/PW_Project/src/PW_Project.ino"
 #define MAX_BRIGHTNESS 255
 
 // Global variable delcarations
@@ -27,22 +31,19 @@ int8_t validSPO2;                     //indicator to show if the SPO2 calculatio
 int32_t heartRate;                    //heart rate value
 int8_t validHeartRate;                //indicator to show if the heart rate calculation is valid
 String data = String(10);             //data to be sent to server
-uint16_t irBuffer[300];               //infrared LED sensor data
-uint16_t redBuffer[300];              //red LED sensor data
+uint16_t irBuffer[100];               //infrared LED sensor data
+uint16_t redBuffer[100];              //red LED sensor data
 uint32_t loop_counter = 0;            //loop counter
 uint32_t validated_hr = 0;            //validated heart rate data
 uint32_t validated_o2 = 0;            //validated o2 data
 uint32_t previous_request = 0;        //time of the previous sensor data request
-uint16_t delay = 1800000;             //ms delay between readings (default 30 minutes)
-uint8_t current_minute = 0;           //current minute
-uint8_t current_hour = 0;             //current hour
-uint8_t constraint_minute_lower = 0;  //constraint minute
-uint8_t constraint_hour_lower = 0;    //constraint hour
-uint8_t constraint_minute_upper = 0;  //constraint minute (upper)
-uint8_t constraint_hour_upper = 0;    //constraint hour (upper)
+uint32_t delay_time = 1800000;        //ms delay between readings (default 30 minutes)
+float current_time = 0;               //current time
+float constraint_time_lower = 0;      //constraint time lower
+float constraint_time_upper = 0;      //constraint time upper
 
 // Synchronous State Machine
-uint8_t state = 0;
+uint8_t state = 2;
 // 0 = Waiting to request measurement, 1 = Request measurement, 2 = Take measurement, 3 = Post measurement
 
 
@@ -60,12 +61,16 @@ void loop() {
   if (state == 0) {
 
     // Update current time
+    current_time = float(Time.hour()) + Time.minute() / 60.00;
 
-    if (millis() - previous_request >= delay) {
-      if ()
-    // Transition to measurement state
-    state = 1;
-    previous_request = millis();
+    // If we are in the acceptable time frame
+    if (current_time > constraint_time_lower && current_time < constraint_time_upper) {
+      // If we've waited enough between measurements
+      if (millis() - previous_request >= delay_time) {
+        // Transition to measurement state
+        state = 1;
+        previous_request = millis();
+        }
     }
     else {
       // Check for updates from Particle cloud
@@ -75,16 +80,30 @@ void loop() {
   // If we are requesting a measurement
   if (state == 1) {
 
-    // Transition to measurement state
-    state = 2;
+    // Enable LED
+    RGB.control(true);
+    RGB.color(0, 0, 255);
+
+    // If the user places their finger on the sensor
+    if (1) {
+      // Transition to measurement state
+      state = 2;
+    }
+    // If it's been 5 minutes and we haven't transitioned yet
+    else if (millis() - previous_request > 300000) {
+      RGB.color(0, 0, 0);
+      state = 0;
+    }
+    
   }
 
   // If we are taking a measurement:
   if (state == 2) {
 
-    bufferLength = 300; //buffer length of 100 stores 4 seconds of samples running at 25sps
+    bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
   
     //read the first 300 samples, and determine the signal range
+    Serial.println("Entering measumrent loop");
     for (byte i = 0 ; i < bufferLength ; i++) {
       while (max30105.available() == false) //do we have new data?
         max30105.check(); //Check the sensor for new data
@@ -94,26 +113,23 @@ void loop() {
         max30105.nextSample(); //We're finished with this sample so move to next sample
     }
 
-    //After gathering 300 new samples recalculate HR and SP02
+    //After gathering 100 new samples recalculate HR and SP02
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-    if(validSPO2) {
-      validated_o2 = spo2;
-    }
-    if(validHeartRate){
-      validated_hr = heartRate;
-    }
 
     // Transition to post state
-    state = 3;
+    //if (validSPO2 && validHeartRate) {
+      state = 3;
+    //}
   }
 
   // If we are posting data to the server
   if (state == 3) {
-      String send_data = String("{ \"beat\": \"") + String(validated_hr) + "\"" + ", \"ox\": " + String(validated_o2) + "}";
+    Serial.println("entering print state");
+      String send_data = String("{ \"beat\": \"") + String(heartRate) + "\"" + ", \"ox\": " + String(spo2) + "}";
       Particle.publish("Reading", String(send_data), PRIVATE);
       Serial.println(send_data);
 
       // Transition to wait state
-      state = 0;
+      state = 2;
   }
 }
--- Particle/PW_Project/src/PW_Project.ino
+++ Particle/PW_Project/src/PW_Project.ino
@@ -1,7 +1,3 @@
-/******************************************************/
-//       THIS IS A GENERATED FILE - DO NOT EDIT       //
-/******************************************************/
-
 #include "Particle.h"
 // ECE 513
 // Author: Nicolas Blanchard
@@ -25,19 +21,19 @@ int8_t validSPO2;                     //indicator to show if the SPO2 calculatio
 int32_t heartRate;                    //heart rate value
 int8_t validHeartRate;                //indicator to show if the heart rate calculation is valid
 String data = String(10);             //data to be sent to server
-uint16_t irBuffer[300];               //infrared LED sensor data
-uint16_t redBuffer[300];              //red LED sensor data
+uint16_t irBuffer[100];               //infrared LED sensor data
+uint16_t redBuffer[100];              //red LED sensor data
 uint32_t loop_counter = 0;            //loop counter
 uint32_t validated_hr = 0;            //validated heart rate data
 uint32_t validated_o2 = 0;            //validated o2 data
 uint32_t previous_request = 0;        //time of the previous sensor data request
-uint32_t delay = 1800000;             //ms delay between readings (default 30 minutes)
+uint32_t delay_time = 1800000;        //ms delay between readings (default 30 minutes)
 float current_time = 0;               //current time
 float constraint_time_lower = 0;      //constraint time lower
 float constraint_time_upper = 0;      //constraint time upper
 
 // Synchronous State Machine
-uint8_t state = 0;
+uint8_t state = 2;
 // 0 = Waiting to request measurement, 1 = Request measurement, 2 = Take measurement, 3 = Post measurement
 
 
@@ -47,9 +43,6 @@ void setup() {
   max30105.begin();
   max30105.setup(60, 4, 2, 100, 411, 4096);
   Serial.println("Setup finished!");
-
-  // LED Pin
-  pinMode(D6, OUTPUT);
 }
 
 void loop() {
@@ -63,7 +56,7 @@ void loop() {
     // If we are in the acceptable time frame
     if (current_time > constraint_time_lower && current_time < constraint_time_upper) {
       // If we've waited enough between measurements
-      if (millis() - previous_request >= delay) {
+      if (millis() - previous_request >= delay_time) {
         // Transition to measurement state
         state = 1;
         previous_request = millis();
@@ -82,22 +75,25 @@ void loop() {
     RGB.color(0, 0, 255);
 
     // If the user places their finger on the sensor
-    if () {
+    if (1) {
       // Transition to measurement state
       state = 2;
     }
-
     // If it's been 5 minutes and we haven't transitioned yet
-    if (millis() - previous_request > 300000)
+    else if (millis() - previous_request > 300000) {
+      RGB.color(0, 0, 0);
+      state = 0;
+    }
     
   }
 
   // If we are taking a measurement:
   if (state == 2) {
 
-    bufferLength = 300; //buffer length of 100 stores 4 seconds of samples running at 25sps
+    bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
   
     //read the first 300 samples, and determine the signal range
+    Serial.println("Entering measumrent loop");
     for (byte i = 0 ; i < bufferLength ; i++) {
       while (max30105.available() == false) //do we have new data?
         max30105.check(); //Check the sensor for new data
@@ -107,26 +103,23 @@ void loop() {
         max30105.nextSample(); //We're finished with this sample so move to next sample
     }
 
-    //After gathering 300 new samples recalculate HR and SP02
+    //After gathering 100 new samples recalculate HR and SP02
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-    if(validSPO2) {
-      validated_o2 = spo2;
-    }
-    if(validHeartRate){
-      validated_hr = heartRate;
-    }
 
     // Transition to post state
-    state = 3;
+    //if (validSPO2 && validHeartRate) {
+      state = 3;
+    //}
   }
 
   // If we are posting data to the server
   if (state == 3) {
-      String send_data = String("{ \"beat\": \"") + String(validated_hr) + "\"" + ", \"ox\": " + String(validated_o2) + "}";
+    Serial.println("entering print state");
+      String send_data = String("{ \"beat\": \"") + String(heartRate) + "\"" + ", \"ox\": " + String(spo2) + "}";
       Particle.publish("Reading", String(send_data), PRIVATE);
       Serial.println(send_data);
 
       // Transition to wait state
-      state = 0;
+      state = 2;
   }
 }
