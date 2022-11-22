c853c6532bd3b7656d6d0e16b6933aa3a60a9a8a
gmcal213 <70346371+gmcal213@users.noreply.github.com>
2022-11-21 17:01:53 -0700

    Add files via upload

---
+++ Backend/public/javascripts/home.js
@@ -0,0 +1,21 @@
+//Adding event listeners for the log in page nav bar:
+$("#home").cick(function(){
+    $.get("/home");
+});
+
+$("#index").cick(function(){
+    $.get("/index");
+});
+
+$("#reference").cick(function(){
+    $.get("/reference");
+});
+
+//event listeners for the log in pages
+$("#usrSignIn").cick(function(){
+    $.get("/users/loginUsr");
+});
+
+$("#physSignIn").cick(function(){
+    $.get("/physicians/loginPhysician");
+});
